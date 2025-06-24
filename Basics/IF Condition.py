1135d96194374a2fd59ffc6a65afd2650b3aeecf
Vegash P <Vegash.P@diggibyte.com>
2025-06-24 18:42:52 +0530

    Basics of Python PART 01

---
+++ Basics/IF Condition.py
@@ -0,0 +1,27 @@
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+
+x = 0
+if x > 0:
+    print("Positive")
+elif x == 0:
+    print("Zero")
+
+
+x = -5
+if x > 0:
+    print("Positive")
+elif x == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+
+#Find the number ODD or EVEN
+x = 10
+if(x%2 == 0):
+    print("Even")
+else:
+    print("Odd")
